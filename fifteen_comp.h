51387d9798bad2712fdc788b56fbaade6a181273
Yuki Sato <sato@asunaro.arc.cs.okayama-u.ac.jp>
2017-11-18 20:20:01 +0900

    Added fifteen_comp module and Modified add_half module

---
+++ fifteen_comp.h
@@ -0,0 +1,16 @@
+
+/* This is code of Floating-Calcuration generator. */
+/*                                                 */
+/* 2017~ Sato Yuki, Okayama Univercity             */
+/* If there is error, please send error report.    */
+
+
+
+declare fifteen_comp{
+  input a<15>;
+  input b<15>;
+  output l;
+  instrin do;
+  
+  instr_arg do(a, b);
+}
