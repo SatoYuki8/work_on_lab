6f70a5c331f0b2ca3f1a07aa5ef2cb21496cdf9e
Yuki Sato <sato@momiji.arc.cs.okayama-u.ac.jp>
2018-01-25 22:23:04 +0900

    SECONDS simulation and FloPoCo test are matched. :)

--- script/flopoco_half.rb
+++ script/flopoco_half.rb
@@ -1,11 +1,11 @@
 arg1 = ARGV.shift
 A = 0..31
-B = 0..2047
+B = 0..31
 time = 1
 for a in A
   for b in B
       if arg1 == "gensec" then
-        print format("set exp %05b; set frac %011b00; forward +1;\n", a, b)
+          print format("set a 0%05b%010b; set b 0%05b%010b; forward +1;\n", a, a*7, b, b*4)
       else
         print format("...");
       end
